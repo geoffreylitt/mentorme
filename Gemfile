e3bcc57c918c3c037291fdfdf24fe93995e25444
Seth Thompson <s3th.thompson@gmail.com>
2013-02-22 08:46:47 -0500

    static page

--- Gemfile
+++ Gemfile
@@ -11,7 +11,6 @@ gem 'sqlite3'
 # in production environments by default.
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
-  gem 'coffee-rails', '~> 3.2.1'
   gem 'haml-rails'
   # See https://github.com/sstephenson/execjs#readme for more supported runtimes
   # gem 'therubyracer', :platforms => :ruby
--- config/routes.rb
+++ config/routes.rb
@@ -55,4 +55,7 @@ Mentorme::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+
+  get "static_pages/home"
+
 end
