8d1276aa18c9dbe5ff4363d729fecc375ee54991
Geoffrey Litt <gklitt@gmail.com>
2013-02-23 01:42:03 +0400

    Basic single-user video working!
    Backend/frontend opentok API interaction seems to be working...
    this commit made in questionable mental state, so double check
    everything

--- app/controllers/video_session_controller.rb
+++ app/controllers/video_session_controller.rb
@@ -1,4 +1,13 @@
 class VideoSessionController < ApplicationController
   def show
+    api_key = KEYS[:open_tok][:key]        # Replace with your OpenTok API key.
+    api_secret = KEYS[:open_tok][:secret]  # Replace with your OpenTok API secret.
+
+    opentok = OpenTok::OpenTokSDK.new api_key, api_secret 
+    session_properties = {OpenTok::SessionPropertyConstants::P2P_PREFERENCE => "disabled"}
+    @api_key = api_key #expose api_key
+    @session = opentok.create_session request.remote_addr, session_properties
+    @token = opentok.generate_token :session_id => @session
+
   end
 end
---
+++ config/initializers/keys.rb
@@ -0,0 +1,8 @@
+KEYS = {}
+
+if Rails.env == 'production'
+  KEYS[:open_tok][:key] = ENV["OPENTOK-KEY"]
+  KEYS[:open_tok][:secret] = ENV["OPENTOK-SECRET"]
+else
+  KEYS = HashWithIndifferentAccess.new(YAML.load_file("#{Rails.root}/config/secrets.yml"))
+end
