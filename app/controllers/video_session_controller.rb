947dde8768cc24c7745fff7cf5f7ba599d0f6065
Geoffrey Litt <gklitt@gmail.com>
2013-02-23 01:53:29 +0400

    hardcoded temp opentok session_id to allow simple multiuser

--- app/controllers/video_session_controller.rb
+++ app/controllers/video_session_controller.rb
@@ -6,7 +6,16 @@ class VideoSessionController < ApplicationController
     opentok = OpenTok::OpenTokSDK.new api_key, api_secret 
     session_properties = {OpenTok::SessionPropertyConstants::P2P_PREFERENCE => "disabled"}
     @api_key = api_key #expose api_key
-    @session = opentok.create_session request.remote_addr, session_properties
+
+    # eventually, the flow should be:
+    # test if we have a opentok session ID for this videochat in the db already.
+    # if yes, use that. if not, generate one using the below code and store it.
+    # for now, we will hardcode the session ID to allow multi user chat
+
+    #@session = opentok.create_session request.remote_addr, session_properties
+
+    @session = "2_MX4yMzAyNzExMn4xMjcuMC4wLjF-RnJpIEZlYiAyMiAxMzo1MDozMiBQU1QgMjAxM34wLjI0MTE3NzU2fg"
+    
     @token = opentok.generate_token :session_id => @session
 
   end
