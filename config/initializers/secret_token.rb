65673b58461e2790ec5e701c0b5c0d55cbe95334
Geoffrey Litt <gklitt@gmail.com>
2013-02-22 16:48:29 +0400

    rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mentorme::Application.config.secret_token = '1a76e099e23f91b09b02f5f5c6e6a91366031718d4694d9145513d1749e54e2ffc11e74c91731508f304beb4e0cea182f3e60e12df47dc171d1b857f09cd0657'
