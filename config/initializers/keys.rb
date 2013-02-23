ea3c43ad79311789da8ad5a2b0fb776c57afbdce
Seth Thompson <s3th.thompson@gmail.com>
2013-02-23 01:03:04 -0500

    delete secrets + add fb auth

--- config/initializers/keys.rb
+++ config/initializers/keys.rb
@@ -4,6 +4,14 @@ if Rails.env == 'production'
   KEYS[:open_tok] = {}
   KEYS[:open_tok][:key] = ENV["OPENTOK-KEY"]
   KEYS[:open_tok][:secret] = ENV["OPENTOK-SECRET"]
+  KEYS[:facebook] = {}
+  KEYS[:facebook][:key] = ENV["FACEBOOK-KEY"]
+  KEYS[:facebook][:secret] = ENV["FACEBOOK-SECRET"]
 else
   KEYS = HashWithIndifferentAccess.new(YAML.load_file("#{Rails.root}/config/secrets.yml"))
+end
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :facebook, KEYS[:facebook][:key], KEYS[:facebook][:secret],
+           :scope => 'email,user_location'
 end
