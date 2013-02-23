f4fcad4d6515895c66f332085d1278aa1fd70b1a
Geoffrey Litt <gklitt@gmail.com>
2013-02-23 18:00:07 +0400

    oh shit committed temp changes, reverting...

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,14 +7,12 @@ class ApplicationController < ActionController::Base
   private
 
   def authorize
-    #unless current_user
-    #  flash[:error] = "Unauthorized action."
-    #  redirect_to root_url
-    #end
+    unless current_user
+      redirect_to root_url
+    end
   end
 
   def current_user
-    #@current_user ||= User.find(session[:user_id]) if session[:user_id]
-    current_user = User.first
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 end
