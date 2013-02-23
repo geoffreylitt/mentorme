62953da39430448211fa1772fbeb30c767938f54
Geoffrey Litt <gklitt@gmail.com>
2013-02-24 03:18:22 +0400

    oops, committed my local workaround AGAIN

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,13 +7,12 @@ class ApplicationController < ActionController::Base
   private
 
   def authorize
-    #unless current_user
-    #  redirect_to root_url
-    #end
+    unless current_user
+      redirect_to root_url
+    end
   end
 
   def current_user
-    #@current_user ||= User.find(session[:user_id]) if session[:user_id]
-    @current_user = User.find(1)
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 end
