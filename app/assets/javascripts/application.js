bd41947e53d474c613abf5cce65d65a712808510
Seth Thompson <s3th.thompson@gmail.com>
2013-02-23 07:17:02 -0500

    facebook callback junk hidden

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,9 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+if (window.location.hash == '#_=_') {
+    window.location.hash = ''; // for older browsers, leaves a # behind
+    history.pushState('', document.title, window.location.pathname); // nice and clean
+    e.preventDefault(); // no page reload
+}
