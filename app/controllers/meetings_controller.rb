8dd1354c7167b9b15975c1d42c284f550544a2fc
Geoffrey Litt <gklitt@gmail.com>
2013-02-23 10:44:18 +0400

    minor naming change

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -8,8 +8,7 @@ class MeetingsController < ApplicationController
     opentok = OpenTok::OpenTokSDK.new api_key, api_secret 
     @api_key = api_key #expose api_key
 
-    @session = @meeting.opentok_session_id
-    #@session = "2_MX4yMzAyNzExMn4xMjcuMC4wLjF-RnJpIEZlYiAyMiAxMzo1MDozMiBQU1QgMjAxM34wLjI0MTE3NzU2fg"
+    @session_id = @meeting.opentok_session_id
 
     @token = opentok.generate_token :session_id => @session
   end
