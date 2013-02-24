9681de03ddc99e6096681b142bad065c5bc96114
Alexandra Qin <alexandraqin@gmail.com>
2013-02-24 15:11:34 +0400

    fixing shit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -47,8 +47,7 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
-    @current_user = User.find(1)
-    #@current_user ||= User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
   
 end
