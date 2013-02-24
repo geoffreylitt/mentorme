1f6109fd42d7f0c0538cbf19d5e40e98d3fe20fc
Seth Thompson <s3th.thompson@gmail.com>
2013-02-24 15:48:58 +0400

    fixes

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,39 +31,3 @@ end
 SKILLS.each do |skill|
   Skill.create(name: skill)
 end
-
-english = Language.find_by_name('English')
-arabic = Language.find_by_name('Arabic')
-law_id = Skill.find_by_name('law').id
-
-max = User.create(first_name: 'Max', last_name: 'Stoller')
-max.languages << english
-max.languages << arabic
-LearnSkill.create(user_id: max.id, skill_id: law_id)
-max.save
-
-alex = User.create(first_name: 'Alex')
-alex.languages << english
-TeachSkill.create(user_id: alex.id, skill_id: law_id)
-alex.save
-
-bob = User.create(first_name: 'Bob')
-bob.languages << arabic
-TeachSkill.create(user_id: bob.id, skill_id: law_id)
-bob.save
-
-health_id = Skill.find_by_name('health').id
-steve = User.create(first_name: 'Steve')
-steve.languages << arabic
-TeachSkill.create(user_id: steve.id, skill_id: health_id)
-steve.save
-
-seth = User.create(first_name: 'Seth')
-seth.languages << arabic
-TeachSkill.create(user_id: seth.id, skill_id: law_id)
-seth.save
-
-jeff = User.create(first_name: 'Jeff')
-jeff.languages << arabic
-TeachSkill.create(user_id: jeff.id, skill_id: law_id)
-jeff.save
